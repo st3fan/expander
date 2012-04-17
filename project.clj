e294a60b3fa39fc62a42d4e968d791d1a978de3f
Stefan Arentz <stefan@arentz.ca>
2012-04-16 21:20:52 -0400

    Initial import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject url-expander "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cheshire "4.0.0"]
+                 [noir "1.2.1"]
+                 [aleph "0.2.1-beta2"]]
+  :main expander.server)
